ea95e5d88a23f0f2e2b773b8a08d95fe64c7eeb7
bmath1019 <bmath1019@gmail.com>
2016-08-11 02:31:24 -0400

    Circles elements appear as time passes

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
 
-var data = []
+var app;
+
+var START = 1;
+var END = 1366;
+var MAX_RADIUS = 50;
+var TRANSITION_DURATION = 1;
 
 d3.queue()
-.defer(d3.json, 'data/us-states.json')
-.defer(d3.json, 'data/candidates.json')
-.awaitAll(function (error, results) {
-  if (error) {throw error; }
-  dataMap = results[0];
-  dataCands = results[1];
+  .defer(d3.json, 'data/us-states.json')
+  .defer(d3.json, 'data/candidates.json')
+  .awaitAll(function (error, results) {
+    if (error) {throw error; }
+    dataMap = results[0];
+    dataCands = results[1];
+    app.initialize(dataCands);
+  });
 
-  var charts = [
-    new Chart('#chart1')
-  ];
-})
+app = {
+  data: [],
+  components: [],
 
-function Chart(selector, variable, title){
+  options: {
+    time: START,
+    filtered: true,
+  },
+
+  initialize: function (data) {
+    app.data = data;
+
+    var parseDate = d3.timeParse("%m/%d/%Y"); //https://bl.ocks.org/d3noob/0e276dc70bb9184727ee47d6dd06e915
+
+    data.forEach(function (d) {
+        d.reg_date = parseDate(d.reg_date)
+      });
+
+    app.components = [
+      new Chart('#chart1')
+    ];
+
+    function incrementTime() {
+      app.options.time += 1;
+      if (app.options.time > END) {
+        app.options.time = START;
+      };
+
+      app.update(); 
+    }
+
+    d3.interval(incrementTime, TRANSITION_DURATION);
+  },
+
+  update: function (){
+    app.components.forEach(function (c) {if (c.update) {c.update(); }});
+  }
+}
+
+function Chart(selector) {
   var chart = this;
 
-  chart.margin = {left: 20, right: 20, top: 20, bottom: 20};
+  margin = {left: 20, right: 20, top: 20, bottom: 20};
 
-  chart.width = 950 - chart.margin.left - chart.margin.right;
-  chart.height = 550 - chart.margin.top - chart.margin.bottom;
+  chart.width = 950 - margin.left - margin.right;
+  chart.height = 550 - margin.top - margin.bottom;
 
   chart.projection = d3.geoAlbersUsa();
 
@@ -33,7 +74,7 @@ function Chart(selector, variable, title){
     .attr('width', chart.width)
     .attr('height', chart.height)
     .append("g")
-    .attr("transform", function(){ return "translate(" + chart.margin.left + "," + chart.margin.top + ")" });
+    .attr("transform", function(){ return "translate(" + margin.left + "," + margin.top + ")" });
 
   chart.svg.selectAll("path")
     .data(dataMap.features)
@@ -44,25 +85,36 @@ function Chart(selector, variable, title){
     .attr("stroke", "black")
     .attr("fill", "#194375");
 
-    points = chart.svg.selectAll("circle")
-      .append("g")
-      .data(dataCands);
-
     chart.update();
-
 }
 
-Chart.prototype.update = function () {
+Chart.prototype = {
+  update: function () {
 
-  var chart = this;
+    var chart = this;
 
-  points.enter()
-    .append("circle")
-    .attr("cx", function (d) { return chart.projection([d.cand_long, d.cand_lat]) [0]; })
-    .attr("cy", function (d) { return chart.projection([d.cand_long, d.cand_lat]) [1]; })
-    .attr("r", "6")
-    .attr("fill", "red")
+    var txData = app.data.slice();
 
+    if(app.options.filtered) {txData = txData.filter(function (d) {
+      return d.time_index <= app.options.time; })};
+
+    points = chart.svg.selectAll("circle").data(txData);
+
+    points
+      .enter()
+      .append("circle")
+      .attr("class","cands")
+      .attr("cx", function (d) { return chart.projection([d.cand_long, d.cand_lat]) [0]; })
+      .attr("cy", function (d) { return chart.projection([d.cand_long, d.cand_lat]) [1]; })
+      .attr("r", "6");
+
+    points.exit().remove()
+
+
+
+    console.log(app.options.time)
+
+  }
 }
 
 
