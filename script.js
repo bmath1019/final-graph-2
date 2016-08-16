186a658d1b9cd42dd1e823179ee6fd917adeae86
bmath1019 <bmath1019@gmail.com>
2016-08-15 20:36:37 -0400

    Add Aggregate Data - Begin time brushing svg

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ var TRANSITION_DURATION = 1;
 d3.queue()
   .defer(d3.json, 'data/us-states.json')
   .defer(d3.json, 'data/candidates.json')
+  .defer(d3.json, 'data/candagg.json')
   .awaitAll(function (error, results) {
     if (error) {throw error; }
     dataMap = results[0];
     dataCands = results[1];
+    dataCandAgg = results[2];
     app.initialize(dataCands);
   });
 
@@ -60,8 +62,20 @@ function Chart(selector) {
 
   margin = {left: 20, right: 20, top: 20, bottom: 20};
 
+  brushMargin = {left:40, right:20, top: 20,bottom:20};
+
+
   chart.width = 950 - margin.left - margin.right;
-  chart.height = 550 - margin.top - margin.bottom;
+  chart.height = 570 - margin.top - margin.bottom;
+
+  chart.brushwidth = 950 - brushMargin.left;
+  chart.brushheight = 80 - brushMargin.top;
+
+  // var zoom = d3.behavior.zoom()
+  //   .translate([0, 0])
+  //   .scale(1)
+  //   .scaleExtent([1, 8])
+  //   .on("zoom", zoomed);
 
   chart.projection = d3.geoAlbersUsa();
 
@@ -71,8 +85,8 @@ function Chart(selector) {
 
   chart.svg = d3.select('#chart1')
     .append('svg')
-    .attr('width', chart.width)
-    .attr('height', chart.height)
+    .attr('width', chart.width + margin.left + margin.right)
+    .attr('height', chart.height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", function(){ return "translate(" + margin.left + "," + margin.top + ")" });
 
@@ -85,7 +99,51 @@ function Chart(selector) {
     .attr("stroke", "black")
     .attr("fill", "#194375");
 
-    chart.update();
+  // brush
+
+  chart.x = d3.scaleLinear()
+    .domain([0, d3.max(dataCandAgg,function (d) {return d.time_index_2; } )])
+    .range([0,chart.width])
+    .nice();  
+
+  chart.y = d3.scaleLinear()
+    .domain([0,d3.max(dataCandAgg,function (d) {return d.cand_agg; } )])
+    .range([chart.brushheight,0])
+    .nice();
+
+  xAxis = d3.axisBottom()
+    .scale(chart.x);
+
+  yAxis = d3.axisLeft()
+    .scale(chart.y)
+
+  chart.svg2 = d3.selectAll("#brushtime")
+    .append('svg')
+    .attr('width',chart.width + brushMargin.left + brushMargin.right)
+    .attr('height',chart.brushheight + brushMargin.top + brushMargin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + brushMargin.left + ',' + brushMargin.top + ')');
+
+  chart.svg2.append('g')
+    .attr('class','x axis')
+    .attr('transform', 'translate(0,' + chart.brushheight + ')')
+    .call(xAxis);
+
+  chart.svg2.append('g')
+    .attr('class','y axis')
+    .call(yAxis)
+
+  area = d3.area()
+    .x(function (d) { return chart.x(d.time_index_2); })
+    .y0(chart.brushheight)
+    .y1(function (d) { return chart.y(d.cand_agg); });
+
+   chart.svg2.append("path")
+      .datum(dataCandAgg)
+      .attr("d", area)
+      .attr("class","brush");  
+
+  chart.update();
 }
 
 Chart.prototype = {
@@ -104,7 +162,7 @@ Chart.prototype = {
     points
       .enter()
       .append("circle")
-      .attr("class","cands")
+      .attr("class",function (d) {return d.cand_par})
       .attr("cx", function (d) { return chart.projection([d.cand_long, d.cand_lat]) [0]; })
       .attr("cy", function (d) { return chart.projection([d.cand_long, d.cand_lat]) [1]; })
       .attr("r", "4")
@@ -112,9 +170,7 @@ Chart.prototype = {
 
     points.exit().remove()
 
-
-
-    console.log(app.options.time)
+    // console.log(app.options.time)
 
   }
 }
